e844d0c72b6cdbeaebb99cd115eded930e7a6a34
hideru <jota.16.stigma.world@gmail.com>
2021-11-10 13:07:05 +0900

    Hello World Succeeded

---
+++ src/main/java/com/example/testspringboot_211110b/TestSpringBoot211110BApplication.java
@@ -0,0 +1,22 @@
+package com.example.testspringboot_211110b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestSpringBoot211110BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBoot211110BApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
---
+++ src/test/java/com/example/testspringboot_211110b/TestSpringBoot211110BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringboot_211110b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBoot211110BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
